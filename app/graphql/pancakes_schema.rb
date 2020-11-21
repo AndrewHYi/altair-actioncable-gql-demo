7fd8a6914040635c484bf68e615d03b3cd656142
Andrew H Yi <andrew.yi50@gmail.com>
2020-11-21 14:08:54 -0500

    chore: setup graphql-ruby subscriptions

--- app/graphql/pancakes_schema.rb
+++ app/graphql/pancakes_schema.rb
@@ -3,10 +3,12 @@
 class PancakesSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
+  subscription(Types::SubscriptionType)
 
   # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
   use GraphQL::Analysis::AST
+  use GraphQL::Subscriptions::ActionCableSubscriptions, redis: Redis.new(url: ENV["REDIS_URL"])
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
