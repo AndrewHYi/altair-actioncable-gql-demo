350acb5de8fbead5ddd484626b4547a108003e8d
Andrew H Yi <andrew.yi50@gmail.com>
2020-11-21 12:40:07 -0500

    chore: tweaks cors and csrf config

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem "bootsnap", ">= 1.4.2", require: false
 gem "pg", ">= 0.18", "< 2.0"
 gem "puma", "~> 4.1"
 gem "rails", "~> 6.0.3", ">= 6.0.3.4"
+gem "rack-cors"
 
 gem "graphql"
 
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module Pancakes
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_controller.forgery_protection_origin_check = false
   end
 end
